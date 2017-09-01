06a67bade3c07e83c95c9bcffb6f04ca433c224e
gness1804 <graham.nessler@gmail.com>
2017-09-01 17:13:33 -0500

    Finish first async exercise

--- my-first-async-io.js
+++ my-first-async-io.js
@@ -1,10 +1,13 @@
 const fs = require('fs');
 
+const callback = (err, data) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+  const result = data.split('\n').length - 1;
+  console.log(result)
+}
+
 const file = process.argv[2];
-const output = fs.readFile(file)
-
-
-
-
-// const count = output.split('\n').length - 1;
-// console.log(count)
+const output = fs.readFile(file, 'utf8', callback)
